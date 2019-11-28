51ff30cda6464dcafc34b638b0ad41331d5c4c36
Daniel Jonathan Pitfield <hq017496@live.reading.ac.uk>
2019-11-28 13:22:23 +0000

    Database configuration file

---
+++ dbConfig.php
@@ -0,0 +1,14 @@
+<?php 
+// Database configuration 
+$dbHost     = "localhost"; 
+$dbUsername = "root"; 
+$dbPassword = "reading_crime_2019"; 
+$dbName     = "crime_mapper"; 
+ 
+// Create database connection 
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName); 
+ 
+// Check connection 
+if ($db->connect_error) { 
+    die("Connection failed: " . $db->connect_error); 
+}
