e33d92344a334e8eb1844880b6f7dd53e0171ec6
Daniel Jonathan Pitfield <hq017496@live.reading.ac.uk>
2019-12-27 15:44:39 +0000

    Submit form

--- SaveMarkers.php
+++ SaveMarkers.php
@@ -1,6 +1,30 @@
 <?php
 require 'dbConfig.php';
 
+// From submit form
+if(isset($_POST['Crime_Type']))
+{
+    $crime_type = $_POST['Crime_Type'];
+}
+
+if(isset($_POST['Description']))
+{
+    $description = $_POST['Description'];
+}
+
+if(isset($_POST['Date']))
+{
+    $date = $_POST['Date'];
+}
+
+if(isset($_POST['Time']))
+{
+    $time = $_POST['Time'];
+}
+
+$date_time = $date . ' ' . $time; // Combine date and time together
+
+// From ajax call
 if(isset($_POST['Latitude']))
 {
     $latitude = $_POST['Latitude'];
@@ -12,7 +36,7 @@ if(isset($_POST['Longitude']))
 }
 
 // Insert information into database
-$sql = "INSERT INTO markers (Crime_Type, Description, Latitude, Longitude)
-		VALUES ('Placeholder', 'Placeholder', '$latitude', '$longitude')";
+$sql = "INSERT INTO markers (Crime_Type, Description, Date_Time, Latitude, Longitude)
+		VALUES ('$crime_type', '$description', '$date_time', '$latitude', '$longitude')";
 $db->query($sql);		
 ?>	
