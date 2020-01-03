56c62b39bf6f743847951f2a0aa4731812f57f10
Daniel Jonathan Pitfield <hq017496@live.reading.ac.uk>
2020-01-03 18:17:11 +0000

    Adding categories to markers + delete markers

--- index.php
+++ index.php
@@ -12,282 +12,7 @@ require 'dbConfig.php'; // Include the database configuration file
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  <!-- For navigation bar icons -->
-
-<style>
-html, body {
-margin:0;
-height: 100%
-width: 100%;
-font-family: Arial;
-}
-
-.icon-bar {
-  width: 100%;
-  background-color: #555;
-  overflow: auto;
-}
-
-.IconBarBtn {
-  border: none;
-  text-align: center;
-  padding: 10px 0;
-  font-weight:bold;
-  font-family:Arial;
-  text-decoration:none;
-  color: white;
-  font-size: 20px;
-  background-color: #555;
-}
-
-.IconBarBtn:hover {
-  background-color: #000;
-}
-
-.IconBarBtn.active {
-  background-color: #4CAF50;
-}
-
-.Map {
-  float: left;
-  width: 25%;	
-}
-
-.Signin {
-  float: right;
-  width: 5%;
-}
-
-.Settings {
-  float: right;
-  width: 20%;
-}
-
-#map {
-  position:absolute;
-  height: calc(100% - 45px);
-  width: 100%;
-}
-
-
-#map2 {
-  float:right;
-  width:55%;
-  height:270px;  
-}
-
-.submit_button {
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  font: bold 15px Arial;
-}
-
-.submit_button:hover {
-  opacity: 0.8;
-}
-
-#description {
-        font-family: Arial;
-        font-size: 15px;
-        font-weight: 300;
-      }
-
-      #infowindow-content .title {
-        font-weight: bold;
-      }
-
-      #infowindow-content {
-        display: none;
-      }
-
-      #map #infowindow-content {
-        display: inline;
-      }
-
-      .pac-card {
-        margin: 10px 10px 0 0;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-        background-color: #fff;
-        font-family: Arial;
-      }
-
-      #pac-container {
-        padding-bottom: 12px;
-        margin-right: 12px;
-      }
-
-      .pac-controls {
-        display: inline-block;
-        padding: 5px 11px;
-      }
-
-      .pac-controls label {
-        font-family: Arial;
-        font-size: 13px;
-        font-weight: 300;
-      }
-
-      #pac-input {
-        background-color: #fff;
-        font-family: Arial;
-        font-size: 15px;
-        font-weight: 300;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 48.5%;
-		height: 40px;
-		text-align: center;
-      }
-
-      #pac-input:focus {
-        border-color: #4d90fe;
-      }
-
-      #title {
-        color: #fff;
-        background-color: #4d90fe;
-        font-size: 25px;
-        font-weight: 500;
-        padding: 6px 12px;
-      }
-	  
-      #target {
-        width: 345px;
-      }
-	  
-.custom_contextmenu {
-  background-color: white;
-  position: fixed;
-  width: 10%;
-  display: none;
-}
-	  
-.custom_contextmenu_btn {
-  cursor: pointer;
-  padding: 8px 15px;		
-  font-weight:bold;
-  font-family:Arial;
-  text-decoration:none;
-  color: white;
-  font-size: 14px;
-  text-align: center;
-  background-color: #555;
-}
-
-.custom_contextmenu_btn:hover {
-  background-color: #000;
-}
-
-.add {
-  background-color: #4CAF50;
-}
-
-.modal {
-  display: none;
-  position: fixed;
-  z-index: 1;
-  padding-top: 100px;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgb(0,0,0);
-  background-color: rgba(0,0,0,0.4);
-}
-
-
-.modal-content {
-  position: relative;
-  background-color: #fefefe;
-  margin: auto;
-  padding: 0;
-  border: 1px solid #888;
-  width: 40%;
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
-  -webkit-animation-name: animatetop;
-  -webkit-animation-duration: 0.4s;
-  animation-name: animatetop;
-  animation-duration: 0.4s
-}
-
-@-webkit-keyframes animatetop {
-  from {top:-300px; opacity:0} 
-  to {top:0; opacity:1}
-}
-
-@keyframes animatetop {
-  from {top:-300px; opacity:0}
-  to {top:0; opacity:1}
-}
-
-.close {
-  color: white;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-
-.modal-header {
-  padding: 2px 18px;
-  background-color: #5cb85c;
-  color: white;
-}
-
-.modal-body {padding: 14px 16px;}
-
-input[type=date], input[type=time], select {
- width:17.5%;
- height:25px;
- font-family:Arial;
- font-size:18px;
- background-color: grey;
- outline: none;
- border: 0;
- border-radius: 3px;
- padding: 5px;
- color: white;
-}
-
-select {
-	width:37%;
-	padding: 0px;
-}
-
-input[type=time] {
-	width:10%;
-}
-
-textarea {
-   resize: none;
-   background-color: grey;
-}	
-
-input[type="date"]::-webkit-calendar-picker-indicator { opacity:1; }
-input[type="date"]::-webkit-inner-spin-button,
-input[type="date"]::-webkit-outer-spin-button,       
-input[type="date"]::-webkit-clear-button { -webkit-appearance: none;display: none; }
-
-input[type="time"]::-webkit-inner-spin-button,
-input[type="time"]::-webkit-outer-spin-button { opacity:1; } 
-input[type="time"]::-webkit-clear-button { -webkit-appearance: none;display: none; }
-	  
-</style>
+<link rel="stylesheet" href="layout.css">  <!-- For navigation bar icons -->
 
 <body oncontextmenu="return false;">  <!-- Disable the default right click context menu for the body of the page -->
 
@@ -324,7 +49,7 @@ input[type="time"]::-webkit-clear-button { -webkit-appearance: none;display: non
 		<input type="time" name="Time" value="00:00" required>
 		<br></br>
 		Type:
-		<select name="Crime_Type">
+		<select id="Crime_Type" name="Crime_Type">
 		<option value="Arson">Arson</option>
 		<option value="Murder">Murder</option>
 		<option value="Anti-social Behaviour">Anti-social Behaviour</option>
@@ -370,19 +95,33 @@ input[type="time"]::-webkit-clear-button { -webkit-appearance: none;display: non
 	*/
 	
 	var MarkerArray = [];	
-	function placeMarker(CenterLocation, map) {
+	function placeMarker(ID,Crime_Type,Description,CenterLocation,map) {
 		var marker = new google.maps.Marker({
+		ID: ID,
+		Crime_Type: Crime_Type,
+		Description: Description,
 		position: CenterLocation,
 		map: map
 		});
 		MarkerArray.push(marker);
-	}
-	
-	function placeDraggableMarker(CenterLocation, map) {
-		var marker = new google.maps.Marker({
-		position: CenterLocation,
-		draggable: true,
-		map: map
+		
+		google.maps.event.addListener(marker, 'click', function() {
+			// Should open small context menu with red option to 'Delete' first, then do the following...
+			// Delete from ...
+			marker.setVisible(false); // View
+			MarkerArray.pop(marker); // Array
+			var MarkerID = marker.ID; // Database
+			//alert(MarkerID);
+
+			$.ajax({
+				url: 'DeleteMarker.php',
+				type: 'POST',
+				data: {MarkerID: MarkerID},
+				success: function(data)
+				{
+					//
+				}
+			});
 		});
 	}
 
@@ -414,19 +153,21 @@ input[type="time"]::-webkit-clear-button { -webkit-appearance: none;display: non
 	function LoadMarkers() {	
 		var markers = [
 			<?php 
-			$result = $db->query("SELECT Latitude, Longitude FROM markers"); // Returns output of statement
+			$result = $db->query("SELECT * FROM markers"); // Returns output of statement
 			if($result->num_rows > 0){ 
 				while($row = $result->fetch_assoc()){ 
-					echo '['.$row['Latitude'].', '.$row['Longitude'].'],'; 
+					echo '['.$row['ID'].', "'.$row['Crime_Type'].'", "'.$row['Description'].'", '.$row['Latitude'].', '.$row['Longitude'].'],';
 				} 
 			} 
 			?>
 		];
 
 		for( i = 0; i < markers.length; i++ ) { // Placing the markers stored in the database
-			var Point = new google.maps.LatLng(markers[i][0], markers[i][1]);
-			// [Marker Number, Latitude],[Marker Number, Longitude]
-			placeMarker(Point,map);		
+			var ID = markers[i][0];
+			var Crime_Type = markers[i][1];
+			var Description = markers[i][2];
+			var Point = new google.maps.LatLng(markers[i][3], markers[i][4]); // [Marker Number, Latitude],[Marker Number, Longitude]
+			placeMarker(ID,Crime_Type,Description,Point,map);		
 		}
 		
 	}
@@ -434,13 +175,13 @@ input[type="time"]::-webkit-clear-button { -webkit-appearance: none;display: non
 	
 	function hideAllMarkers(){
 		for(i=0; i < MarkerArray.length; i++){
-			MarkerArray[i].setMap(null);
+			MarkerArray[i].setVisible(false);
 		}
 	}
 	
 	function showAllMarkers(){
 		for(i=0; i < MarkerArray.length; i++){
-			MarkerArray[i].setMap(map);
+			MarkerArray[i].setVisible(true);
 		}
 	}
 
@@ -541,7 +282,14 @@ input[type="time"]::-webkit-clear-button { -webkit-appearance: none;display: non
 	
 	$("#submit_form").submit(function(e) {
 		e.preventDefault();
-			
+		
+		/* Take values locally */				
+		var dropdown = document.getElementById("Crime_Type"); // Initial step of getting crime type
+		
+		var Crime_Type = dropdown.options[dropdown.selectedIndex].value;
+		var Description = document.getElementById("description").value;
+
+		/* Also send to database */	
 		var formData = $("#submit_form").serialize();
 		
 		var Vars = {Latitude: Latitude, Longitude: Longitude};
@@ -553,21 +301,20 @@ input[type="time"]::-webkit-clear-button { -webkit-appearance: none;display: non
 			url: 'SaveMarkers.php',
 			type: 'POST',
 			data: data,
-			success: function(data)
+			success: function(result)
 			{
-				// Success message
+				//alert(result); // Result is the id being returned
+				if (SmallMarkerMoved == true) {
+					placeMarker(result,Crime_Type,Description,SecondLocation,map); // Place a static marker on the main map
+				}
+				else {
+					placeMarker(result,Crime_Type,Description,FirstLocation,map); // Place a static marker on the main map
+				}
+				SmallMarkerMoved = false;
+				modal_add.style.display = "none"; // Close input window
 			}
+			
 		});
-		
-		if (SmallMarkerMoved == true) {
-			placeMarker(SecondLocation,map); // Place a static marker on the main map
-		}
-		else {
-			placeMarker(FirstLocation,map); // Place a static marker on the main map
-		}
-		SmallMarkerMoved = false;
-
-		modal_add.style.display = "none"; // Close input window
 	});
 
 	span_add.onclick = function() { // Close button for add crime input window
