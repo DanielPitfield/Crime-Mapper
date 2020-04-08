a5081a97204ee5d2ae752bac9f392af69a404a66
Daniel Jonathan Pitfield <hq017496@live.reading.ac.uk>
2020-04-08 21:44:25 +0100

    SQLi rework

--- dbConfig.php
+++ dbConfig.php
@@ -6,10 +6,10 @@ $dbPassword = "crime_mapper_2019";
 $dbName     = "hq017496_crime_mapper"; 
  
 // Create database connection 
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName); 
+$db = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName); 
  
 // Check connection 
-if ($db->connect_error) { 
-    die("Connection failed: " . $db->connect_error); 
+if (!$db) { 
+    die("Connection failed: " . $db->connect_error()); 
 }
 ?>
