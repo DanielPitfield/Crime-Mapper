d93e4559360d9ecf1fdba2e21f502024d83a0de0
Daniel Jonathan Pitfield <hq017496@live.reading.ac.uk>
2020-04-09 11:34:58 +0100

    SQLi Rework

--- ImportMarkers.php
+++ ImportMarkers.php
@@ -12,6 +12,14 @@ if(isset($_POST['imp_Crime_Date']))
     $date = $_POST['imp_Crime_Date'];
 }
 
+if(isset($_POST['imp_Crime_Time']))
+{
+    $time = $_POST['imp_Crime_Time'];
+}
+else {
+    $time = '00:00:00';
+}
+
 if(isset($_POST['imp_Description']))
 {
     $description = $_POST['imp_Description'];
@@ -27,11 +35,13 @@ if(isset($_POST['imp_Longitude']))
     $longitude = $_POST['imp_Longitude'];
 }
 
-// Insert information into database
-$sql = "INSERT INTO markers (Crime_Type, Crime_Date, Crime_Time, Description, Latitude, Longitude)
-		VALUES ('$crime_type', '$date', '00:00:00', '$description', '$latitude', '$longitude')";
-$db->query($sql);
+$stmt = $db->prepare('INSERT INTO markers (Crime_Type, Crime_Date, Crime_Time, Description, Latitude, Longitude)
+		VALUES (?,?,?,?,?,?)');
 
-$id= mysqli_insert_id($db);
+$stmt->bind_param('ssssdd', $crime_type, $date, $time, $description, $latitude, $longitude);
+
+$stmt->execute();
+
+$id = mysqli_insert_id($db);
 echo $id;	
 ?>
